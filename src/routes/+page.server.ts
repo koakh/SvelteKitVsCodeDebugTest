f528aedced180731c1f25d30fcc633fecfe52d90
Mário Monteiro <marioammonteiro@gmail.com>
2023-03-31 23:11:24 +0100

    clean up page and use Zero-effort type safety

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,4 @@
-import type { PageServerLoad } from './$types';
-
 export const load = (async () => {
   const message = 'hello debugger';
   return { message };
-}) satisfies PageServerLoad;
+});
--- src/routes/debug/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load = (async () => {
-  const message = 'hello debugger';
-  // eslint-disable-next-line no-debugger
-  debugger;
-  return { message };
-}) satisfies PageServerLoad;
